3fc1e9e1b4e1e836c082a9f9331828cc6d896316
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2023-07-23 20:42:38 -0300

    d

--- react-calculator/src/pages/index.tsx
+++ react-calculator/src/pages/index.tsx
@@ -3,15 +3,38 @@
  
  export default function Home() {
   let [visor, setVisor] = useState('')
+  let Igual: boolean = false
+  
   function Numbers(props: string){
     return(setVisor(visor += props))
     
   }
-  function Calculate(props: string){
-    // numero: number = () => (Numbers).toNumber
-    // if (props == 'x')
+  
+  function Equal(props: boolean){
+    if (true){
+
+    }
   }
 
+  //Preparar a função das contas
+
+  function Calculate(props: string){
+    let numero1: number = Number(visor)
+    let numero2: number
+    let resultado: string
+    setVisor(visor = '')
+    if(Igual == true) {
+      numero2 = Number(visor)
+      setVisor(visor = '')
+      if(props == 'x'){
+        let resultadoNumero: any = (numero1 * numero2)
+        resultado = toString(resultadoNumero)
+        setVisor(visor = resultado)
+      } 
+    }
+
+
+  }
   return (
     <div className="bg-black w-[355px]  h-screen m-auto">
       <div className="flex flex-col  p-6 ">
@@ -43,18 +66,18 @@
           <button onClick={() => Numbers('9')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             9
           </button>
-          <button className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Calculate('x')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           x
           </button>
         </div>
         <div className="flex justify-between my-4">
-        <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+        <button onClick={() => Numbers('4')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             4
           </button>
-          <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+          <button onClick={() => Numbers('5')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             5
           </button>
-          <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+          <button onClick={() => Numbers('6')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             6
           </button>
           <button className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
@@ -62,13 +85,13 @@
           </button>
         </div>
         <div className="flex justify-between my-4">
-        <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+        <button onClick={() => Numbers('1')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             1
           </button>
-          <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+          <button onClick={() => Numbers('2')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             2
           </button>
-          <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+          <button onClick={() => Numbers('3')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             3
           </button>
           <button className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
@@ -76,14 +99,14 @@
           </button>
         </div>
         <div className="flex justify-between my-4">
-        <button className="bg-neutral-700 rounded-full text-center basis-36 h-16 text-white font-semibold text-xl">
+        <button onClick={() => Numbers('0')} className="bg-neutral-700 rounded-full text-center basis-36 h-16 text-white font-semibold text-xl">
             0
           </button>
  
           <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             ,
           </button>
-          <button className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Equal(true)} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           =
           </button>
         </div>
