4e5a5973ff17ff964e1910294ffae30f53fb43d2
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2023-07-24 19:59:37 -0300

    Calculadora semi-pronta

--- react-calculator/src/pages/index.tsx
+++ react-calculator/src/pages/index.tsx
@@ -2,39 +2,58 @@
 
  
  export default function Home() {
-  let [visor, setVisor] = useState('')
-  let Igual: boolean = false
+  let [visor, setVisor]:any  = useState('0')
+  let [operator, setOperator]:any = useState('')
+  let [number1, setnumber1]:any = useState(0)
+  let [number2, setnumber2]:any = useState(0)
   
   function Numbers(props: string){
-    return(setVisor(visor += props))
-    
+    if(visor == '0'){
+      setVisor(visor = '')
+      return(setVisor(visor += props))
+    }
+    else{
+      return(setVisor(visor += props))
+    }
   }
   
-  function Equal(props: boolean){
-    if (true){
+  function Operator(props: string){
+    setnumber1(number1 = parseFloat(visor))
+    setVisor(visor = '0')
+    return (setOperator(operator = props))
+  }
 
+  function Calculate(){
+    setnumber2(number2 = parseFloat(visor))
+    if (operator == 'x'){
+      setVisor(visor = number1 * number2)
+    }
+    else if (operator == '+'){
+      setVisor(visor = number1 + number2)
+    }
+    else if (operator == '/'){
+      if(number2 == 0){
+        setVisor(visor = 'Dividir por zero, parça??')
+      }
+      else{
+        setVisor(visor = number1 / number2)
+      }
+    }
+    else if (operator == '-'){
+      setVisor(visor = number1 - number2)
     }
   }
 
-  //Preparar a função das contas
-
-  function Calculate(props: string){
-    let numero1: number = Number(visor)
-    let numero2: number
-    let resultado: string
-    setVisor(visor = '')
-    if(Igual == true) {
-      numero2 = Number(visor)
-      setVisor(visor = '')
-      if(props == 'x'){
-        let resultadoNumero: any = (numero1 * numero2)
-        resultado = toString(resultadoNumero)
-        setVisor(visor = resultado)
-      } 
-    }
-
-
+  function CleanAll(){
+    setVisor(visor = '0')
+    setOperator(operator = '')
+    setnumber1(number1 = null)
+    setnumber2(number2 = null)
   }
+
+
+
+  
   return (
     <div className="bg-black w-[355px]  h-screen m-auto">
       <div className="flex flex-col  p-6 ">
@@ -43,7 +62,7 @@
       </div>
       <div className="">
         <div className="flex justify-between my-4">
-          <button className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
+          <button onClick={() => CleanAll()} className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
             AC
           </button>
           <button className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
@@ -52,7 +71,7 @@
           <button className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
             %
           </button>
-          <button className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('/')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           ÷
           </button>
         </div>
@@ -66,7 +85,7 @@
           <button onClick={() => Numbers('9')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             9
           </button>
-          <button onClick={() => Calculate('x')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('x')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           x
           </button>
         </div>
@@ -80,7 +99,7 @@
           <button onClick={() => Numbers('6')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             6
           </button>
-          <button className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('-')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           -
           </button>
         </div>
@@ -94,7 +113,7 @@
           <button onClick={() => Numbers('3')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             3
           </button>
-          <button className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('+')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           +
           </button>
         </div>
@@ -106,7 +125,7 @@
           <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             ,
           </button>
-          <button onClick={() => Equal(true)} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Calculate()} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           =
           </button>
         </div>
