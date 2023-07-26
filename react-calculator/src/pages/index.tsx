0afa29c3bd42b3bcd5a5fe8333197ad85b4a1385
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2023-07-26 16:44:05 -0300

    calculadora 90% pronta

--- react-calculator/src/pages/index.tsx
+++ react-calculator/src/pages/index.tsx
@@ -6,6 +6,7 @@
   let [operator, setOperator]:any = useState('')
   let [number1, setnumber1]:any = useState(null)
   let [number2, setnumber2]:any = useState(null)
+  let [virgula, setVirgula]:any = useState(0)
   
   function Numbers(props: string){
     if(visor == '0'){
@@ -13,14 +14,31 @@
       return(setVisor(visor += props))
     }
     else{
-      return(setVisor(visor += props))
+      if(visor.length <= 17){
+        return(setVisor(visor += props))
+      }
     }
   }
   
-  function Operator(props: string){
+  function Operator(props: string) {
     setnumber1(number1 = parseFloat(visor))
     setVisor(visor = '0')
     return (setOperator(operator = props))
+  }
+
+  function Virgula() {
+    if(virgula == 0){
+      setVirgula(virgula =+ 1)
+      setVisor(visor += '.')
+    }
+  }
+
+  function MaisMenos(){
+    setVisor(visor = visor * -1)
+  }
+
+  function Porcentagem(){
+    setVisor(visor = visor / 100)
   }
 
   function Calculate(){
@@ -49,15 +67,16 @@
     setOperator(operator = '')
     setnumber1(number1 = null)
     setnumber2(number2 = null)
+    setVirgula(virgula = 0)
   }
 
 
 
   
   return (
-    <div className="bg-black w-[355px]  h-screen m-auto">
-      <div className="flex flex-col  p-6 ">
-      <div id="Visor" className="text-slate-50 text-6xl flex flex-row-reverse font-mono">
+    <div className="bg-black w-[355px] h-screen m-auto ">
+      <div className="flex flex-col p-7 my-5">
+      <div id="Visor" className="text-slate-50 text-5xl flex flex-row-reverse font-mono ">
         {visor}
       </div>
       <div className="">
@@ -65,13 +84,13 @@
           <button onClick={() => CleanAll()} className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
             AC
           </button>
-          <button className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
+          <button onClick={() => MaisMenos()} className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
             +/-
           </button>
-          <button className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
+          <button onClick={() => Porcentagem()} className="bg-slate-200 rounded-full text-center w-16 h-16 text-black font-semibold text-xl">
             %
           </button>
-          <button onClick={() => Operator('/')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('/')} id="dividir" className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           ÷
           </button>
         </div>
@@ -85,7 +104,7 @@
           <button onClick={() => Numbers('9')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             9
           </button>
-          <button onClick={() => Operator('x')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('x')} id='multi' className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           x
           </button>
         </div>
@@ -99,12 +118,12 @@
           <button onClick={() => Numbers('6')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             6
           </button>
-          <button onClick={() => Operator('-')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('-')} id='menos' className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           -
           </button>
         </div>
         <div className="flex justify-between my-4">
-        <button onClick={() => Numbers('1')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+          <button onClick={() => Numbers('1')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             1
           </button>
           <button onClick={() => Numbers('2')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
@@ -113,7 +132,7 @@
           <button onClick={() => Numbers('3')} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             3
           </button>
-          <button onClick={() => Operator('+')} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
+          <button onClick={() => Operator('+')} id='mais' className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
           +
           </button>
         </div>
@@ -122,7 +141,7 @@
             0
           </button>
  
-          <button className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
+          <button onClick={() => Virgula()} className="bg-neutral-700 rounded-full text-center w-16 h-16 text-white font-semibold text-xl">
             ,
           </button>
           <button onClick={() => Calculate()} className="bg-orange-500 text-white rounded-full text-center w-16 h-16 font-semibold text-xl">
